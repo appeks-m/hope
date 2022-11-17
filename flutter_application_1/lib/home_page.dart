433de9218959be380b13d78a2537f5f318e6c753
appeks-m <113274025+appeks-m@users.noreply.github.com>
2022-11-17 07:50:48 +0330

    Squashed commit of the following:
    commit 7398ee07067c0541770b0d90800f304a82995be2
    Author: nargessbakhshy <113302889+nargessbakhshy@users.noreply.github.com>
    Date:   Wed Nov 16 17:43:50 2022 +0330
    
        first run

---
+++ flutter_application_1/lib/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _homepage createState() => _homepage();
+}
+
+class _homepage extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
